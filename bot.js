a5fb313d19e8ec48da4af2bd636c161afb45bbf5
ahlatn02 <44965902+ahlatn02@users.noreply.github.com>
2019-04-08 14:53:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,7 +209,7 @@ client.on("message", async message => {
 		message.channel.send("Overlord Gaiden Prologue: Vampire of a Ruined Country");
         message.channel.send("Link: https://meobietbay.com/light-novel/overlord/overlord-gaiden/overlord-gaiden-mo-dau/");
     } 	
-	https://meobietbay.com/light-novel/overlord/overlord-gaiden/overlord-gaiden-mo-dau/
+
 	
 });
 
